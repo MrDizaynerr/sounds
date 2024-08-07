879756d530cad7b6b0bbf450f4257b26e63d5303
vyacheslav <viktorov.slava@inbox.ru>
2024-08-08 09:43:52 +1000

    Просто мейн

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from data.atomic import keysenum as ke
 
 
 def main0():
-    g: Gamma = Gamma(ke.KeyEnum.C, True)
+    g: Gamma = Gamma(ke.KeyEnum.G, True)
+    print("Соль мажор: ", end="")
     print(str(g))
-    print(g.get_step(0))
-    print([str(c) for c in g.get_natural_chord()])
-    print([str(c) for c in g.get_cept_chord()])
+    print("Натуральный аккорд: " + str([str(c) for c in g.get_natural_chord()]))
+    print("Септаккорд" + str([str(c) for c in g.get_cept_chord()]))
+    print("Квинта: " + str([str(c) for c in g.get_quint()]))
+
+    g = Gamma(ke.KeyEnum.G, major=False)
+    print("Соль минор: ", end="")
+    print(str(g))
+    print("Натуральный аккорд: " + str([str(c) for c in g.get_natural_chord()]))
+    print("Септаккорд: " + str([str(c) for c in g.get_cept_chord()]))
+    print("Квинта: " + str([str(c) for c in g.get_quint()]))
 
 main: list = [main0]
 
