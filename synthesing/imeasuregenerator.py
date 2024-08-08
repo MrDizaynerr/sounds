80be42731a09bd878bbde46a5afb4bdce89a1dad
Viktorov Vyacheslav <viktorov.slava@inbox.ru>
2024-08-08 21:21:16 +1000

    Абстрактный метод для кривой экспрессии

--- synthesing/imeasuregenerator.py
+++ synthesing/imeasuregenerator.py
@@ -14,5 +14,15 @@ class IMeasureGenerator(ABC):
         """
         pass
 
+    @abstractmethod
+    def _generate_keys_weights(self, sub_beats_count: int) -> list[float]:
+        """
+        Генерирует кривую экспрессии, которая задаёт, в общем смысле,
+        насколько много нот и будет в каждой доле такта.
+        :sub_beats_count количество долей, для которых будет вестись расчёт
+        :return последовательность чисел размером с количество долей, значения которых в пределах от 0 до 1
+        """
+        return [.5 for _ in range(sub_beats_count)]
+
     def __init__(self, size: int):
         self._size = size
