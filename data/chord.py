bbb6710d6bc6b82c58a5bf8efa3b6f3bff400e05
vyacheslav <viktorov.slava@inbox.ru>
2024-08-09 11:21:20 +1000

    В бите теперь не список нот, а аккорд

--- data/chord.py
+++ data/chord.py
@@ -5,23 +5,29 @@ class Chord:
     """
     Хранит аккорды. В частности, ноты, которые в него входят и его название
     """
-    keys: list[k.Key]
-    name: str
+    _keys: list[k.Key]
+    _name: str
 
     def __init__(self, keys: list[k.Key]) -> None:
         key_len: int = len(keys)
         if key_len < 2:
             raise ValueError(f"Аккорд не может состоять менее, чем из двух нот ({key_len})!")
-        self.keys = keys
-        self.name = keys[0].name
+        self._keys = keys
+        self._name = keys[0].name
         if key_len > 2:
             first_second_difference: int = keys[1] - keys[0]
             if first_second_difference == 3:
-                self.name += "m"
+                self._name += "m"
             if key_len == 4:
                 third_four_difference: int = keys[3] - keys[2]
                 if third_four_difference == 3:
-                    self.name += "7"
+                    self._name += "7"
+
+    def get_keys(self):
+        return self._keys
+
+    def get_name(self):
+        return self._name
 
     def __str__(self):
-        return self.name
+        return self._name
--- synthesing/beat.py
+++ synthesing/beat.py
@@ -1,5 +1,6 @@
 from data.atomic import key as k
 from data import gamma as g
+from data import chord as c
 
 
 class Beat:
@@ -7,8 +8,8 @@ class Beat:
     Тип, представляющий собой долю в такте.
     Хранит ноты, которые должны во время доли быть сыграны и информацию о положении в такте.
     """
-    # ноты
-    _keys: list[k.Key]
+    # аккорд (частный случай - нота)
+    _keys: c.Chord
     # положение доли в такте
     _position: float
 
@@ -17,7 +18,7 @@ class Beat:
         Проверяет, все ли ноты такта принадлежат заданной гамме
         TODO: надо бы переработать это. Потому что сейчас оно точно не покрывает все случаи. Те же септы тут упадут в False. Или я просто чего то не понимаю
         """
-        for key in self._keys:
+        for key in self._keys.get_keys():
             if key not in gamma.get_key_list():
                 return False, key
         return True, None
@@ -28,7 +29,7 @@ class Beat:
         """
         return self._position
 
-    def __init__(self, keys: list[k.Key], position: float) -> None:
+    def __init__(self, keys: c.Chord, position: float) -> None:
         """
         :keys - может быть как одиночной нотой, так и аккордом
         """
